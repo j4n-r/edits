fd07b880f8839ef46efde5e3f55f1302ce16dd92
j4n-r <jan.ruegge@gmail.com>
2025-07-26 16:16:31 +0200

    draw whole screen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::io::Read;
+use std::{io::Read, sync::LazyLock};
+
+static EDITOR_CONFIG: std::sync::LazyLock<EditorConfig> = LazyLock::new(|| EditorConfig {
+    orig_termios: enable_raw_mode().expect("Enabling raw mode failed"),
+    window: get_window_size().expect("Error getting window size"),
+});
 
 fn main() -> std::io::Result<()> {
-    let _ = Cleanup {
-        orig_termios: enable_raw_mode().expect("enabling raw mode failed"),
-    };
-    enable_raw_mode()?;
+    println!("{}", *EDITOR_CONFIG);
     process_key()?;
     Ok(())
 }
@@ -19,16 +21,25 @@ fn read_key() -> Result<u8, std::io::Error> {
 
 fn process_key() -> Result<(), std::io::Error> {
     loop {
+        refresh_screen();
         let c = read_key()?;
+        if c == ctrl_key(b'q') {
+            print!("\x1b[2J");
+            print!("\x1b[H");
+            return Ok(());
+        }
         if is_ctrl(c) {
             print!("{}\r\n", c);
         } else {
             print!("{}\r\n", c as char);
         }
-        if c == ctrl_key(b'q') {
-            std::process::exit(1);
-        }
     }
+}
+
+fn refresh_screen() {
+    draw_rows();
+    print!("\x1b[2J");
+    print!("\x1b[H");
 }
 
 fn enable_raw_mode() -> Result<libc::termios, std::io::Error> {
@@ -51,6 +62,13 @@ fn enable_raw_mode() -> Result<libc::termios, std::io::Error> {
     }
 }
 
+fn draw_rows() {
+    for _i in 0..EDITOR_CONFIG.window.ws_row - 1 {
+        print!("~\r\n")
+    }
+    print!("~");
+}
+
 fn disable_raw_mode(termios: libc::termios) -> Result<(), std::io::Error> {
     unsafe {
         if libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &termios) != 0 {
@@ -66,12 +84,33 @@ const fn ctrl_key(k: u8) -> u8 {
     k & 0x1f
 }
 
-struct Cleanup {
-    orig_termios: libc::termios,
+fn get_window_size() -> Result<libc::winsize, std::io::Error> {
+    unsafe {
+        let ws: libc::winsize = std::mem::zeroed();
+        if libc::ioctl(libc::STDOUT_FILENO, libc::TIOCGWINSZ, &ws) == -1 {
+            return Err(std::io::Error::last_os_error());
+        }
+        Ok(ws)
+    }
 }
 
-impl Drop for Cleanup {
+struct EditorConfig {
+    orig_termios: libc::termios,
+    window: libc::winsize,
+}
+
+impl Drop for EditorConfig {
     fn drop(&mut self) {
         let _ = disable_raw_mode(self.orig_termios).expect("disabling raw mode failed");
+    }
+}
+
+impl std::fmt::Display for EditorConfig {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "(Cols: {}, Rows: {})",
+            self.window.ws_col, self.window.ws_row
+        )
     }
 }
